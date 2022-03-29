8540e1d519bcdd712f255f4a99b705e46c046487
Gaëtan Massart <gtn.massart@gmail.com>
2022-03-29 09:37:35 +0200

    mise en place Landing page

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 import { NavComponent } from './nav/nav.component';
 
 const routes: Routes = [
+  { path: '', component: LandingPageComponent },
   { path: 'nav', component: NavComponent }
 ];
 
