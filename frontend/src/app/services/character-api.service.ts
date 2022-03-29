858082e6805cb55c9f65297ac2952d63d858c1dc
Gaëtan Massart <gtn.massart@gmail.com>
2022-03-29 11:46:50 +0200

    last update

---
+++ frontend/src/app/services/character-api.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { ICharacter } from '../models/icharacter';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CharacterApiService {
+
+  private _url = "http://localhost:3000/data/"
+
+  constructor(private _httpClient: HttpClient) { }
+
+  public getAll(): Observable<ICharacter[]> {
+    return this._httpClient.get<ICharacter[]>(this._url);
+  }
+
+  public get(id: number): Observable<ICharacter> {
+    return this._httpClient.get<ICharacter>(this._url + id);
+  }
+}
